f7633c51f78fa6e5d5656141065d3be5a4ff2530
LizPillajo <ldpillajo@uce.edu.ec>
2025-11-10 05:58:01 -0500

    Changes in server.js to load the front end

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express, { json } from "express";
 import fetch from "node-fetch";
 import cors from "cors";
@@ -5,7 +7,12 @@ import cors from "cors";
 const app = express();
 const PORT = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../")));
 
 app.get("/", (req, res) => {
     res.send("Disney Characters Backend Server running")
